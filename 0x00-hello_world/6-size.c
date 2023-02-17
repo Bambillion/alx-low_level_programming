c6f7b831f33ac86dfaa9f336bba8b7af17630c80
Seye <info2deskibro@gmail.com>
2023-02-17 13:49:05 +0000

    Error on 6-size checked

--- 0x00-hello_world/6-size.c
+++ 0x00-hello_world/6-size.c
@@ -13,11 +13,11 @@ int main(void)
 	long int l;
 	long long int n;
 
-	printf("Size of a char: %lu  byte(s)\n",(unsigned long)sizeof(c));
-	printf("Size of an int: %lu byte(s)\n",(unsigned long)sizeof(i));
-	printf("Size of a long int: %lu byte(s)\n",(unsigned long)sizeof(l));
-	printf("Size 
-of a long long int: %lu byte(s)\n",(unsigned long)sizeof(n));
-	printf("Size of a float: %zu byte(s)\n",sizeof(float));
+	printf("Size of a char: %lu byte(s)\n", (unsigned long)sizeof(c));
+	printf("Size of an int: %lu byte(s)\n", (unsigned long)sizeof(i));
+	printf("Size of a long int: %lu byte(s)\n", (unsigned long)sizeof(l));
+	printf("Size of a long long int: %lu byte(s)\n", (unsigned long)sizeof(n));
+	printf("Size of a float: %zu byte(s)\n", sizeof(float));
+	return (0);
 }
 
