a90ce9ed80a693f24e29655037614590104126ea
Seye <info2deskibro@gmail.com>
2023-02-17 13:18:21 +0000

    100-intel script executed

--- 0x00-hello_world/6-size.c
+++ 0x00-hello_world/6-size.c
@@ -10,13 +10,14 @@ int main(void)
 {
 	int i;
 	char c;
-	float f;
 	long int l;
-	long long int n;;
+	long long int n;
 
-	printf("Size of an int: %lu  byte(s)\n",(unsigned long)sizeof(i));
-	printf("Size of a char: %lu byte(s)\n", (unsigned long)sizeof(c));
-	printf("Size of a float: %lu byte(s)\n", (unsigned long)sizeof(f));
-	printf("Size of a long int: %lu byte(s)\n", (unsigned long)sizeof(l));
-	printf("Size of a long long int: %lu byte(s)\n",(unsigned long)sizeof(n));
+	printf("Size of a char: %lu  byte(s)\n",(unsigned long)sizeof(c));
+	printf("Size of an int: %lu byte(s)\n",(unsigned long)sizeof(i));
+	printf("Size of a long int: %lu byte(s)\n",(unsigned long)sizeof(l));
+	printf("Size 
+of a long long int: %lu byte(s)\n",(unsigned long)sizeof(n));
+	printf("Size of a float: %zu byte(s)\n",sizeof(float));
 }
+
