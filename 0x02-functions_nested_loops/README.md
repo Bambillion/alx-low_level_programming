bec4fad25198391959566b58580569b34f9128ef
Seye <info2deskibro@gmail.com>
2023-02-22 15:48:42 +0000

    Add 0-putchar.c script

---
+++ 0x02-functions_nested_loops/README.md
@@ -0,0 +1,6 @@
+# 0x02-functions_nested_loops
+<br>
+# About Me
+<br>
+
+**0-putchar.c:** Write a program that prints _putchar, followed by a new line
---
+++ 0x02-functions_nested_loops/main.h
@@ -0,0 +1,14 @@
+#ifndef _MAIN_H
+#define _MAIN_H
+
+/**
+  * _putchar - writes the character c to std
+  *
+  * @c: parameter input
+  *
+  * Return: 1 or 0
+  */
+
+int _putchar(char c);
+
+#endif
