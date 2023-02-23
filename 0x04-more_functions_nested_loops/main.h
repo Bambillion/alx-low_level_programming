02fd0f83e9aeb2f862fb3b91e69353423b2a3937
Seye <info2deskibro@gmail.com>
2023-02-23 20:50:21 +0000

    Add 4-print_most_numbers script

--- 0x04-more_functions_nested_loops/main.h
+++ 0x04-more_functions_nested_loops/main.h
@@ -48,4 +48,12 @@ int mul(int a, int b);
 
 void print_numbers(void);
 
+/**
+  * print_most_numbers - print numbers from 0 to 9 except
+  * '2' and '4'
+  * Return: 0 always
+  */
+
+void print_most_numbers(void);
+
 #endif
