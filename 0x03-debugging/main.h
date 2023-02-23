7499701cf2f34c0113c63dc34955f15717b76a5c
Seye <info2deskibro@gmail.com>
2023-02-23 23:31:55 +0000

    Add 2-largest_number file

--- 0x03-debugging/main.h
+++ 0x03-debugging/main.h
@@ -12,4 +12,14 @@
 
 void positive_or_negative(int i);
 
+/**
+  * largest_number - this function returns the largest value
+  * of three integers
+  * @a: The first integer
+  * @b:The second integer to be compared
+  * @c:The third integer to be compared
+  */
+
+int largest_number(int a, int b, int c);
+
 #endif /* MAIN_H */
