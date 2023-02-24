b395d32731226d341368672842e8522fdb8e27f3
Seye <info2deskibro@gmail.com>
2023-02-24 04:09:58 +0000

    Leap year script

--- 0x03-debugging/main.h
+++ 0x03-debugging/main.h
@@ -22,4 +22,25 @@ void positive_or_negative(int i);
 
 int largest_number(int a, int b, int c);
 
+/**
+  * print_remaining_days - this function prints how many days are
+  * left in the year
+  * @month: month in number format
+  * @day: day of the month
+  * @year: year
+  * Return: void
+  */
+
+void print_remaining_days(int month, int day, int year);
+
+/**
+  * -converts day of month to day of year without 
+  * accounting for leap year
+  * @month: month in number 
+  * @day: day of month
+  * Return: day of year
+  */
+
+int convert_day(int month, int day);
+
 #endif /* MAIN_H */
