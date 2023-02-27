3c882a6817c79d687f282346fb57d55b1e780423
Seye <info2deskibro@gmail.com>
2023-02-27 22:14:21 +0000

    Add 2-strlen script

--- 0x05-pointers_arrays_strings/main.h
+++ 0x05-pointers_arrays_strings/main.h
@@ -27,4 +27,11 @@ void reset_to_98(int *n);
   */
 void swap_int(int *a, int *b);
 
+/**
+  * _strlen - string length
+  * @s: string
+  * Return: return the value of string length
+  */
+
+int _strlen(char *s);
 #endif
