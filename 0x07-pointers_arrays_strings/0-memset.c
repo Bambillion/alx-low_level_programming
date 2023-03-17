f6062185174e4e382ea852c0b7d84bceff613acc
Seye <info2deskibro@gmail.com>
2023-03-17 13:07:07 +0000

    Add 0-memset.c script

---
+++ 0x07-pointers_arrays_strings/0-memset.c
@@ -0,0 +1,22 @@
+#include "main.h"
+#include "_putchar.c"
+/**
+  * _memset - write n bytes of char b to pointer s
+  * @s: memory area to be filled
+  * @b: hexa code to write memory
+  * @n : size of memory area to be filled
+  * Return: returns pointer to memory area s
+  */
+
+char *_memset(char *s, char b, unsigned int n)
+{
+
+	unsigned int i = 0;
+
+	while (i < n)
+	{
+		s[i] = _putchar(b);
+		i++;
+	}
+	return (s);
+}
